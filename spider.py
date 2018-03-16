e9b3d9b3f8c6402354364eacc749f674986e3141
wscjxky <619677632@qq.com>
2018-03-16 19:12:01 +0800

    init

--- spider.py
+++ spider.py
@@ -5,7 +5,6 @@ import re
 import os
 import socket
 import time
-import urllib.request
 from bs4 import BeautifulSoup
 import redis
 
@@ -20,8 +19,14 @@ M_Headers = {
 
 }
 socket.setdefaulttimeout(10)
-DB = redis.Redis(host='47.94.251.202', port=6379, db=4, password='wscjxky')
+DB = redis.Redis(host='47.94.251.202', port=6379, db=4, password='wscjxky',decode_responses=True)
 key_cache="paper:"
+def getPaperLinkdata(data):
+    soup = BeautifulSoup(data, 'html.parser')
+    tags_div = soup.find_all('div', itemprop="album-mainEntityOfPage")
+    for tag_div in tags_div:
+        print(tag_div)
+    # pic = re.findall(u'img  src="(.*?)"', data)
 def requestUrl(url, sort, restart=True):
     # proxy={'http': 'http://39.134.93.13:80'}
     # proxy_support = urllib2.ProxyHandler(proxy)
@@ -32,17 +37,18 @@ def requestUrl(url, sort, restart=True):
         if restart:
             if DB.get(filename):
                 return DB.get(filename)
-            # else:
-            #     # print url
-            #     time.sleep(TIME_SLEEP)
-            #     req = urllib.request.Request(url, headers=M_Headers)
-            #     data = urllib.request.urlopen(req).read()
-            #     DB.set(filename, data)
-            #     return data
+            else:
+                # print url
+                time.sleep(TIME_SLEEP)
+                req = urllib.request.Request(url, headers=M_Headers)
+                data = urllib.request.urlopen(req).read()
+                DB.set(filename, data)
+                return data
 
     except urllib.HTTPError as e:
             if (e.code == 503):
                 STOP_FLAG = True
 if __name__ == '__main__':
     url='https://www.researchgate.net/profile/Gongpu_Wang'
-    print(requestUrl(url,url[-10:]))
+    data=(requestUrl(url,url[-10:]))
+    getPaperLinkdata(data)
